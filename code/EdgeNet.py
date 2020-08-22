7f850c6ab751b312cc7de4cde21347df282acc2d
stsan9 <stsan@ucsd.edu>
2020-08-22 03:15:03 +0000

    module

---
+++ code/EdgeNet.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import EdgeConv
+
+class EdgeNet(nn.Module):
+    def __init__(self, input_dim=4, big_dim=32, hidden_dim=2, aggr='mean'):
+        super(EdgeNet, self).__init__()
+        encoder_nn = nn.Sequential(nn.Linear(2*(input_dim), big_dim),
+                               nn.ReLU(),
+                               nn.Linear(big_dim, big_dim),
+                               nn.ReLU(),
+                               nn.Linear(big_dim, hidden_dim),
+                               nn.ReLU(),
+        )
+        
+        decoder_nn = nn.Sequential(nn.Linear(2*(hidden_dim), big_dim),
+                               nn.ReLU(),
+                               nn.Linear(big_dim, big_dim),
+                               nn.ReLU(),
+                               nn.Linear(big_dim, input_dim)
+        )
+        
+        self.batchnorm = nn.BatchNorm1d(input_dim)
+
+        self.encoder = EdgeConv(nn=encoder_nn,aggr=aggr)
+        self.decoder = EdgeConv(nn=decoder_nn,aggr=aggr)
+
+    def forward(self, data):
+        data.x = self.batchnorm(data.x)
+        data.x = self.encoder(data.x,data.edge_index)
+        data.x = self.decoder(data.x,data.edge_index)
+        return data.x
