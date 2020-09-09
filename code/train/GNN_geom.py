08204975707f024d4109c0b98ab80d6ea2911195
stsan9 <stsan@ucsd.edu>
2020-09-08 23:38:49 -0700

    can specify hidden dimension and if including E

--- code/train/GNN_geom.py
+++ code/train/GNN_geom.py
@@ -40,7 +40,7 @@ class EdgeNet(nn.Module):
 
 # train and test helper functions
 @torch.no_grad()
-def test(model,loader,total,batch_size):
+def test(model,loader,total,batch_size, no_E = False):
     model.eval()
     
     mse = nn.MSELoss(reduction='mean')
@@ -49,6 +49,8 @@ def test(model,loader,total,batch_size):
     t = tqdm.tqdm(enumerate(loader),total=total/batch_size)
     for i,data in t:
         data = data.to(device)
+        if (no_E == True):
+            data.x = data.x[:,:-1]
         y = data.x # the model will overwrite data.x, so save a copy
         batch_output = model(data)
         batch_loss_item = mse(batch_output, y).item()
@@ -58,7 +60,7 @@ def test(model,loader,total,batch_size):
 
     return sum_loss/(i+1)
 
-def train(model, optimizer, loader, total, batch_size):
+def train(model, optimizer, loader, total, batch_size, no_E = False):
     model.train()
     
     mse = nn.MSELoss(reduction='mean')
@@ -67,6 +69,8 @@ def train(model, optimizer, loader, total, batch_size):
     t = tqdm.tqdm(enumerate(loader),total=total/batch_size)
     for i,data in t:
         data = data.to(device)
+        if (no_E == True):
+            data.x = data.x[:,:-1]
         y = data.x # the model will overwrite data.x, so save a copy
         optimizer.zero_grad()
         batch_output = model(data)
@@ -80,72 +84,78 @@ def train(model, optimizer, loader, total, batch_size):
     
     return sum_loss/(i+1)
 
-# data and specifications
-gdata = GraphDataset(root='/anomalyvol/data/gnn_node_global_merge', bb=0)
-input_dim = 4
-big_dim = 32
-hidden_dim = 2
-fulllen = len(gdata)
-tv_frac = 0.10
-tv_num = math.ceil(fulllen*tv_frac)
-batch_size = 4
-n_epochs = 100
-lr = 0.001
-patience = 10
-device = 'cuda:0'
-model_fname = 'GNN_AE_EdgeConv'
+def __name__ == "__main__":
+    import argparse
 
-model = EdgeNet(input_dim=input_dim, big_dim=big_dim, hidden_dim=hidden_dim).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+    parser.add_argument("hid_dim", type=int, help="latent space size", required=True)
+    parser.add_argument("no_E", type=bool, help="Bool to remove energy from training and testing", required=True)
+    args = parser.parse_args()
+    # data and specifications
+    gdata = GraphDataset(root='/anomalyvol/data/gnn_node_global_merge', bb=0)
+    input_dim = 4
+    big_dim = 32
+    hidden_dim = args.hid_dim
+    fulllen = len(gdata)
+    tv_frac = 0.10
+    tv_num = math.ceil(fulllen*tv_frac)
+    batch_size = 4
+    n_epochs = 100
+    lr = 0.001
+    patience = 10
+    device = 'cuda:0'
+    model_fname = 'GNN_AE_EdgeConv'
 
-def collate(items): # collate function for data loaders (transforms list of lists to list)
-    l = sum(items, [])
-    return Batch.from_data_list(l)
+    model = EdgeNet(input_dim=input_dim, big_dim=big_dim, hidden_dim=hidden_dim).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr)
 
-# train, valid, test split
-torch.manual_seed(0) # lock seed for random_split
-train_dataset, valid_dataset, test_dataset = random_split(gdata, [fulllen-2*tv_num,tv_num,tv_num])
-train_loader = DataListLoader(train_dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
-train_loader.collate_fn = collate
-valid_loader = DataListLoader(valid_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
-valid_loader.collate_fn = collate
-test_loader = DataListLoader(test_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
-test_loader.collate_fn = collate
+    def collate(items): # collate function for data loaders (transforms list of lists to list)
+        l = sum(items, [])
+        return Batch.from_data_list(l)
 
-train_samples = len(train_dataset)
-valid_samples = len(valid_dataset)
-test_samples = len(test_dataset)
+    # train, valid, test split
+    torch.manual_seed(0) # lock seed for random_split
+    train_dataset, valid_dataset, test_dataset = random_split(gdata, [fulllen-2*tv_num,tv_num,tv_num])
+    train_loader = DataListLoader(train_dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
+    train_loader.collate_fn = collate
+    valid_loader = DataListLoader(valid_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
+    valid_loader.collate_fn = collate
+    test_loader = DataListLoader(test_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
+    test_loader.collate_fn = collate
 
-# create model
-model = EdgeNet(input_dim=input_dim, big_dim=big_dim, hidden_dim=hidden_dim).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr = lr)
-# load in model
-modpath = osp.join('/anomalyvol/models/',model_fname+'.best.pth')
-try:
-    model.load_state_dict(torch.load(modpath))
-except:
-    pass
+    train_samples = len(train_dataset)
+    valid_samples = len(valid_dataset)
+    test_samples = len(test_dataset)
 
-# Training loop
-stale_epochs = 0
-best_valid_loss = test(model, valid_loader, valid_samples, batch_size)
-for epoch in range(0, n_epochs):
-    loss = train(model, optimizer, train_loader, train_samples, batch_size)
-    valid_loss = test(model, valid_loader, valid_samples, batch_size)
-    print('Epoch: {:02d}, Training Loss:   {:.4f}'.format(epoch, loss))
-    print('               Validation Loss: {:.4f}'.format(valid_loss))
+    # create model
+    model = EdgeNet(input_dim=input_dim, big_dim=big_dim, hidden_dim=hidden_dim).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+    # load in model
+    modpath = osp.join('/anomalyvol/models/',model_fname+'.best.pth')
+    try:
+        model.load_state_dict(torch.load(modpath))
+    except:
+        pass
 
-    if valid_loss < best_valid_loss:
-        best_valid_loss = valid_loss
-        modpath = osp.join('/anomalyvol/models/',model_fname+'.best.pth')
-        print('New best model saved to:',modpath)
-        torch.save(model.state_dict(),modpath)
-        stale_epochs = 0
-    else:
-        print('Stale epoch')
-        stale_epochs += 1
-    if stale_epochs >= patience:
-        print('Early stopping after %i stale epochs'%patience)
-        break
-        
-print("Completed")
+    # Training loop
+    stale_epochs = 0
+    best_valid_loss = test(model, valid_loader, valid_samples, batch_size)
+    for epoch in range(0, n_epochs):
+        loss = train(model, optimizer, train_loader, train_samples, batch_size, args.no_E)
+        valid_loss = test(model, valid_loader, valid_samples, batch_size, args.no_E)
+        print('Epoch: {:02d}, Training Loss:   {:.4f}'.format(epoch, loss))
+        print('               Validation Loss: {:.4f}'.format(valid_loss))
+
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            modpath = osp.join('/anomalyvol/models/',model_fname+'.best.pth')
+            print('New best model saved to:',modpath)
+            torch.save(model.state_dict(),modpath)
+            stale_epochs = 0
+        else:
+            print('Stale epoch')
+            stale_epochs += 1
+        if stale_epochs >= patience:
+            print('Early stopping after %i stale epochs'%patience)
+            break
+            
+    print("Completed")
