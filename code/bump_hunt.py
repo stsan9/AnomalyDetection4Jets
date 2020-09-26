978a499668b468aa809e0c50db1a73f605d9ec94
stsan9 <stsan@ucsd.edu>
2020-09-26 08:19:22 +0000

    roc curve

--- code/bump_hunt.py
+++ code/bump_hunt.py
@@ -3,6 +3,7 @@ Generate graphs for bump hunting on invariant mass.
 """
 import glob
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import torch
 import os.path as osp
 import models
@@ -13,6 +14,7 @@ from torch.nn import MSELoss
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from sklearn import metrics
 
 def invariant_mass(jet1_e, jet1_px, jet1_py, jet1_pz, jet2_e, jet2_px, jet2_py, jet2_pz):
     """
@@ -93,7 +95,7 @@ def process(data_loader, num_events, model_fname, model_num, use_sparseloss, lat
         use_sparseloss (bool): toggle for using sparseloss instead of mse
 
     Returns: torch.tensor of size (num_events, 5).
-             column-wise: [jet1_loss, jet2_loss, dijet_invariant_mass, jet1_mass, jet2_mass]
+             column-wise: [jet1_loss, jet2_loss, dijet_invariant_mass, jet1_mass, jet2_mass, rnd_truth_bit]
              Row-wise: dijet of event
         
     """
@@ -111,7 +113,7 @@ def process(data_loader, num_events, model_fname, model_num, use_sparseloss, lat
 
     # Store the return values
     max_feat = 4
-    jets_loss_mass = torch.zeros((num_events, 5), dtype=torch.float32)
+    jets_proc_data = torch.zeros((num_events, 6), dtype=torch.float32)
     input_fts = []
     reco_fts = []
     event = -1 # event counter
@@ -144,14 +146,16 @@ def process(data_loader, num_events, model_fname, model_num, use_sparseloss, lat
                     # calculate invariant mass (data.u format: p[event_idx, n_particles, jet.mass, jet.px, jet.py, jet.pz, jet.e]])
                     jet0_u = data[i].u[0]
                     jet1_u = data[i+1].u[0]
+                    
                     dijet_mass = invariant_mass(jet0_u[6], jet0_u[3], jet0_u[4], jet0_u[5],
                                                 jet1_u[6], jet1_u[3], jet1_u[4], jet1_u[5])
-                    jet_losses = torch.tensor([loss_ftn(jet_rec_0, jet_x_0), # loss of jet 1
+                    jets_info = torch.tensor([loss_ftn(jet_rec_0, jet_x_0), # loss of jet 1
                                                loss_ftn(jet_rec_1, jet_x_1), # loss of jet 2
                                                dijet_mass,              # mass of dijet
                                                jet0_u[2],               # mass of jet 1
-                                               jet1_u[2]])              # mass of jet 2
-                    jets_loss_mass[event,:] = jet_losses
+                                               jet1_u[2],               # mass of jet 2
+                                               jet1_u[-1]])             # if this event was an anomaly
+                    jets_proc_data[event,:] = jets_info
                     input_fts.append(jet_x_0)
                     input_fts.append(jet_x_1)
                     reco_fts.append(jet_rec_0)
@@ -169,67 +173,81 @@ def process(data_loader, num_events, model_fname, model_num, use_sparseloss, lat
                     jet1_u = data[i+1].u[0]
                     dijet_mass = invariant_mass(jet0_u[6], jet0_u[3], jet0_u[4], jet0_u[5],
                                                 jet1_u[6], jet1_u[3], jet1_u[4], jet1_u[5])
-                    jet_losses = torch.tensor([loss_ftn(jet_rec_0, jet_x_0), # loss of jet 1
+                    jets_info = torch.tensor([loss_ftn(jet_rec_0, jet_x_0), # loss of jet 1
                                                loss_ftn(jet_rec_1, jet_x_1), # loss of jet 2
                                                dijet_mass,              # mass of dijet
                                                jet0_u[2],               # mass of jet 1
-                                               jet1_u[2]])              # mass of jet 2
-                    jets_loss_mass[event,:] = jet_losses
+                                               jet1_u[2],               # mass of jet 2
+                                               jet1_u[-1]])             # if this event was an anomaly
+                    jets_proc_data[event,:] = jets_info
                     input_fts.append(jet_x_0)
                     input_fts.append(jet_x_1)
                     reco_fts.append(jet_rec_0)
                     reco_fts.append(jet_rec_1)
                 
     # return pytorch tensors
-    return jets_loss_mass[:event], torch.cat(input_fts), torch.cat(reco_fts)
+    return jets_proc_data[:event], torch.cat(input_fts), torch.cat(reco_fts)
 
 
-def bump_hunt(jet_losses, cuts, model_fname, model_num, use_sparseloss, bb):
+def bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, bb):
     """
     Loops and makes multiple cuts on the jet losses, and generates a graph for each cut by
     delegating to make_bump_graph().
 
     Args:
-        jet_losses (torch.tensor): output of process(); has loss and mass of jets per event
+        proc_jets (torch.tensor): output of process(); has loss and mass of jets per event
         cuts (list of floats): all the percentages to perform a cut on the loss
         model_fname (str): name of saved model
         model_num (int): 0 for EdgeConv based models, 1 for MetaLayer based models
         use_sparseloss (bool): toggle for using sparseloss instead of mse
         bb (str): which black box the bump hunt is being performed on (e.g. 'bb1')
     """
-    losses = jet_losses[:,:2].flatten().numpy()
-    loss_graph_name = model_fname + '_sparseloss' if use_sparseloss == True else model_fname
-    make_loss_graph(losses, model_fname + '/' + loss_graph_name + '_loss_distribution_' + bb)
+    savedir = model_fname + '/' + bb + '/'
+    Path('/anomalyvol/figures/' + savedir).mkdir(exist_ok=True) # make a subfolder
+    losses = proc_jets[:,:2].flatten().numpy()
+    make_loss_graph(losses,  savedir + 'loss_distribution')
+    cut_props = {'cut': [], 'tpr': [], 'fpr': []} # use to generate table if anoms known
     # generate a graph for different cuts
     for cut in cuts:
-        loss_thresh = np.quantile(losses, cut)
-        d = {'loss1': jet_losses[:,0],
-             'loss2': jet_losses[:,1],
-             'dijet_mass': jet_losses[:,2],
-             'mass1': jet_losses[:,3],
-             'mass2': jet_losses[:,4]}
-        df = pd.DataFrame(d)
-        all_dijet_mass = df['dijet_mass']
-        df['outlier'] = 0
-        # outlier dijet if either jet is an outlier
-        df.loc[(df['loss1'] > loss_thresh) | (df['loss2'] > loss_thresh), 'outlier'] = 1
-        outliers = df.loc[df.outlier == 1]
-
-        # name for files when saved
+        # name for graph files when saved
         dijet_graph_name = ""
         mj1_graph_name = ""
         mj2_graph_name = ""
         if use_sparseloss == True:
-            dijet_graph_name = model_fname + '/' + model_fname + '_withsparseloss_dijet_bump_' + bb + '_' + str(cut)
-            mj1_graph_name = model_fname + '/' + model_fname + '_withsparseloss_mj1_bump_' + bb + '_' + str(cut)
-            mj2_graph_name = model_fname + '/' + model_fname + '_withsparseloss_mj2_bump_' + bb + '_' + str(cut)
+            dijet_graph_name = savedir + 'dijet_bump_' + str(cut)
+            mj1_graph_name = savedir + 'mj1_bump_' + str(cut)
+            mj2_graph_name = savedir + 'mj2_bump_' + str(cut)
         else:
-            dijet_graph_name = model_fname + '/' + model_fname + '_dijet_bump_' + bb + '_' + str(cut)
-            mj1_graph_name = model_fname + '/' + model_fname + '_mj1_bump_' + bb + '_' + str(cut)
-            mj2_graph_name = model_fname + '/' + model_fname + '_mj2_bump_' + bb + '_' + str(cut)
+            dijet_graph_name = savedir + 'dijet_bump_' + str(cut)
+            mj1_graph_name = savedir + 'mj1_bump_' + str(cut)
+            mj2_graph_name = savedir + 'mj2_bump_' + str(cut)
+
+        loss_thresh = np.quantile(losses, cut)
+        d = {'loss1': proc_jets[:,0],
+             'loss2': proc_jets[:,1],
+             'dijet_mass': proc_jets[:,2],
+             'mass1': proc_jets[:,3],
+             'mass2': proc_jets[:,4],
+             'truth_bit': proc_jets[:,5]}
+        df = pd.DataFrame(d)
+        df['outlier'] = 0
+        # classify dijet as outlier if either jet is an outlier
+        df.loc[(df['loss1'] > loss_thresh) | (df['loss2'] > loss_thresh), 'outlier'] = 1
+        outliers = df.loc[df.outlier == 1]
+        positives = len(df.loc[df.truth_bit == 1])
+        negatives = len(df.loc[df.truth_bit == 0])
 
         # make dijet bump hunt graph
+        all_dijet_mass = df['dijet_mass']
         outlier_dijet_mass = outliers['dijet_mass'] # get the mass of only outliers
+        if bb == 'rnd': # gather info for roc curve for rnd dataset
+            num_caught_anoms = len(outliers.loc[outliers.truth_bit == 1])
+            tpr = num_caught_anoms / positives
+            fpr = (len(outliers) - num_caught_anoms) / negatives
+            cut_props['cut'].append(cut)
+            cut_props['tpr'].append(tpr)
+            cut_props['fpr'].append(fpr)
+
         x_lab = '$m_{jj}$ [GeV]'
         bins = np.linspace(1000, 6000, 51)
         make_bump_graph(all_dijet_mass, outlier_dijet_mass, x_lab, dijet_graph_name, bins)
@@ -248,6 +266,23 @@ def bump_hunt(jet_losses, cuts, model_fname, model_num, use_sparseloss, bb):
         bins = np.linspace(0, 1800, 51)
         make_bump_graph(all_m2_mass, outlier_m2_mass, x_lab, mj2_graph_name, bins)
 
+    if bb == 'rnd':  # plot roc for rnd set
+        tpr = cut_props['tpr']
+        fpr = cut_props['fpr']
+        auc = metrics.auc(tpr, fpr)
+        plt.figure(figsize=(6,4.4))
+        lw = 2
+        plt.plot(fpr, tpr, color='darkorange',
+                     lw=lw, label='ROC curve (area = %0.2f)' % auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.legend(loc="lower right")
+        plt.savefig('/anomalyvol/figures/' + savedir + '_roc.pdf')
+        plt.close()
+
 def plot_reco_difference(input_fts, reco_fts, model_fname, bb):
     """
     Plot the difference between the autoencoder's reconstruction and the original input
@@ -263,7 +298,7 @@ def plot_reco_difference(input_fts, reco_fts, model_fname, bb):
     feat = ['px', 'py', 'pz' , 'E']
 
     # make a separate plot for each feature
-    for i in range(len(input_fts.shape[1])):
+    for i in range(input_fts.shape[1]):
         plt.figure(figsize=(6,4.4))
         bins = np.linspace(-20, 20, 101)
         if i == 3:  # different bin size for E momentum
@@ -288,8 +323,6 @@ if __name__ == "__main__":
     parser.add_argument("--model_num", type=int, help="0 = EdgeConv, 1 = MetaLayer", required=True)
     parser.add_argument("--use_sparseloss", type=int, help="Toggle use of sparseloss (0: False, 1: True). Default 0.", default=0, required=False)
     parser.add_argument("--num_events", type=int, help="How many events to process (multiple of 100). Default 1mil", default=1000000, required=False)
-    parser.add_argument("--cuts", nargs='+', type=float, default=[0.97, 0.99, 0.997, 0.999],
-                        help="All the cuts to use. Float in range (0, 1). Example: --cuts 0.97 0.99 0.995", required=False)
     parser.add_argument("--latent_dim", type=int, help="How many units for the latent space (def=2)", default=2, required=False)
     args = parser.parse_args()
 
@@ -302,42 +335,53 @@ if __name__ == "__main__":
         exit("--model_name does not exist. Valid names are:\n" + str(saved_models))
     if args.use_sparseloss not in [0, 1]:
         exit("--use_sparseloss can only be 0 (for False) or 1 (True)")
-    if any(c <= 0. or c >= 1. for c in args.cuts):
-        exit("--cuts must be in range (0, 1)")
     if args.latent_dim <= 0:
         exit("--latent_dim must be greater than 0")
     model_fname = args.model_name
     model_num = args.model_num
     use_sparseloss = [False, True][args.use_sparseloss]
     num_events = args.num_events
-    cuts = args.cuts
     latent_dim = args.latent_dim
 
     Path('/anomalyvol/figures/' + model_fname).mkdir(exist_ok=True) # make a folder for the graphs of this model
     
-    print("Plotting bb0")
-    bb0 = GraphDataset('/anomalyvol/data/lead_2/bb0/', bb=0)
+    print("Plotting RnD set")
+    cuts = np.arange(0.90, 1.0, 0.01)
+    bb4 = GraphDataset('/anomalyvol/data/lead_2/rnd/', bb=4)
     torch.manual_seed(0) # consistency for random_split
+    num_files = int(10000 - (10000 * (1100000 - num_events) / 1100000)) # how many files to read
+    if num_events == 1000000:    # account for fact rnd set has 100k more events then other boxes
+        num_files = int(10000 - (10000 * (1100000 - (num_events + 100000)) / 1100000)) # how many files to read
+    ignore_files = 10000 - num_files
+    bb4, ignore, ignore2 = random_split(bb4, [num_files, ignore_files, 0])
+    bb4_loader = DataListLoader(bb4)
+    proc_jets, input_fts, reco_fts = process(bb4_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
+    # plot_reco_difference(input_fts, reco_fts, model_fname, 'bb4')  # plot reconstruction difference
+    bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, 'rnd')  # plot bump hunts
+
+    print("Plotting bb0")
+    cuts = np.arange(0.99, 0.999, 0.001)
+    bb0 = GraphDataset('/anomalyvol/data/lead_2/bb0/', bb=0)
     num_files = int(10000 - (10000 * (1000000 - num_events) / 1000000)) # how many files to read
     ignore_files = 10000 - num_files
     bb0, ignore, ignore2 = random_split(bb0, [num_files, ignore_files, 0])
     bb0_loader = DataListLoader(bb0)
-    jet_losses, input_fts, reco_fts = process(bb0_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
-    plot_reco_difference(input_fts, reco_fts, model_fname, 'bb0')  # plot reconstruction difference
-    bump_hunt(jet_losses, cuts, model_fname, model_num, use_sparseloss, 'bb0')  # plot bump hunts
+    proc_jets, input_fts, reco_fts = process(bb0_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
+    # plot_reco_difference(input_fts, reco_fts, model_fname, 'bb0')
+    bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, 'bb0')
 
     print("Plotting bb1")
     bb1 = GraphDataset('/anomalyvol/data/lead_2/bb1/', bb=1)
     bb1, ignore, ignore2 = random_split(bb1, [num_files, ignore_files, 0])
     bb1_loader = DataListLoader(bb1)
-    jet_losses, input_fts, reco_fts = process(bb1_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim)
-    plot_reco_difference(input_fts, reco_fts, model_fname, 'bb1')
-    bump_hunt(jet_losses, cuts, model_fname, model_num, use_sparseloss, 'bb1')
+    proc_jets, input_fts, reco_fts = process(bb1_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim)
+    # plot_reco_difference(input_fts, reco_fts, model_fname, 'bb1')
+    bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, 'bb1')
 
     print("Plotting bb2")
     bb2 = GraphDataset('/anomalyvol/data/lead_2/bb2/', bb=2)
     bb2, ignore, ignore2 = random_split(bb2, [num_files, ignore_files, 0])
     bb2_loader = DataListLoader(bb2)
-    jet_losses, input_fts, reco_fts = process(bb2_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim)
-    plot_reco_difference(input_fts, reco_fts, model_fname, 'bb2')
-    bump_hunt(jet_losses, cuts, model_fname, model_num, use_sparseloss, 'bb2')
+    proc_jets, input_fts, reco_fts = process(bb2_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim)
+    # plot_reco_difference(input_fts, reco_fts, model_fname, 'bb2')
+    bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, 'bb2')
