1bf1c92058cb8208718e8a24ee64044fb0ed6d41
Javier M. Duarte <jduarte@ucsd.edu>
2020-09-28 06:24:41 +0000

    update with dict pkl

--- code/bump_hunt.py
+++ code/bump_hunt.py
@@ -51,12 +51,12 @@ def sparseloss3d(x,y):
     loss = torch.sum(in_dist_out.values + out_dist_in.values) / num_parts
     return loss
 
-def make_bump_graph(all_mass, outlier_mass, x_lab, save_name, bins, output_dir):
+def make_bump_graph(nonoutlier_mass, outlier_mass, x_lab, save_name, bins, output_dir):
     """
     Create matplotlib graphs, overlaying histograms of invariant mass for outliers and all events.
 
     Args:
-        all_mass (tensor): inv mass of jets from all event
+        nonoutlier_mass (tensor): inv mass of jets from nonoutlier event
         outlier_mass (tensor): inv mass from outlier events
         x_lab (str): x axis label for graph
         save_name (str): what name to save graph pdf as
@@ -66,8 +66,8 @@ def make_bump_graph(all_mass, outlier_mass, x_lab, save_name, bins, output_dir):
     plt.figure(figsize=(6,4.4))
     weights = np.ones_like(outlier_mass) / len(outlier_mass)
     plt.hist(outlier_mass, alpha = 0.5, bins=bins, weights=weights, label='Outlier events')
-    weights = np.ones_like(all_mass) / len(all_mass)
-    plt.hist(all_mass, alpha = 0.5, bins=bins, weights=weights, label='All events')
+    weights = np.ones_like(nonoutlier_mass) / len(nonoutlier_mass)
+    plt.hist(nonoutlier_mass, alpha = 0.5, bins=bins, weights=weights, label='Nonoutlier events')
     plt.legend()
     plt.xlabel(x_lab, fontsize=16)
     plt.ylabel('Normalized events [a. u.]', fontsize=16)
@@ -190,13 +190,13 @@ def process(data_loader, num_events, model_fname, model_num, use_sparseloss, lat
     return jets_proc_data[:event], torch.cat(input_fts), torch.cat(reco_fts)
 
 
-def bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, bb, output_dir):
+def bump_hunt(df, cuts, model_fname, model_num, use_sparseloss, bb, output_dir):
     """
     Loops and makes multiple cuts on the jet losses, and generates a graph for each cut by
     delegating to make_bump_graph().
 
     Args:
-        proc_jets (torch.tensor): output of process(); has loss and mass of jets per event
+        df (pd.DataFrame): output of process() transformed into datafram; has loss and mass of jets per event
         cuts (list of floats): all the percentages to perform a cut on the loss
         model_fname (str): name of saved model
         model_num (int): 0 for EdgeConv based models, 1 for MetaLayer based models
@@ -205,9 +205,9 @@ def bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, bb, outpu
     """
     savedir = model_fname + '/' + bb + '/'
     Path(osp.join(output_dir,savedir)).mkdir(exist_ok=True) # make a subfolder
-    losses = proc_jets[:,:2].flatten().numpy()
+    losses = np.concatenate([df['loss1'], df['loss2']])
     make_loss_graph(losses,  savedir + 'loss_distribution', output_dir)
-    cut_props = {'cut': [], 'tpr': [], 'fpr': []} # use to generate table if anoms known
+
     # generate a graph for different cuts
     for cut in cuts:
         # name for graph files when saved
@@ -224,63 +224,59 @@ def bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, bb, outpu
             mj2_graph_name = savedir + 'mj2_bump_' + str(cut)
 
         loss_thresh = np.quantile(losses, cut)
-        d = {'loss1': proc_jets[:,0],
-             'loss2': proc_jets[:,1],
-             'dijet_mass': proc_jets[:,2],
-             'mass1': proc_jets[:,3],
-             'mass2': proc_jets[:,4],
-             'truth_bit': proc_jets[:,5]}
-        df = pd.DataFrame(d)
-        df['outlier'] = 0
-        # classify dijet as outlier if either jet is an outlier
-        df.loc[(df['loss1'] > loss_thresh) | (df['loss2'] > loss_thresh), 'outlier'] = 1
-        outliers = df.loc[df.outlier == 1]
-        positives = len(df.loc[df.truth_bit == 1])
-        negatives = len(df.loc[df.truth_bit == 0])
+        # classify dijet as outlier if both jets are outliers
+        df['outlier'] = (np.minimum(df['loss1'], df['loss2']) > loss_thresh)
+        outliers = df[df['outlier']]
+        # otherwise, classify dijet as nonoutlier
+        nonoutliers = df[~df['outlier']]
+        # alternative definition:
+        #df['nonoutlier'] = (np.maximum(df['loss1'], df['loss2']) < loss_thresh)
+        #nonoutliers = df[df['nonoutlier']]
 
         # make dijet bump hunt graph
         all_dijet_mass = df['dijet_mass']
+        nonoutlier_dijet_mass = nonoutliers['dijet_mass']
         outlier_dijet_mass = outliers['dijet_mass'] # get the mass of only outliers
-        if bb == 'rnd': # gather info for roc curve for rnd dataset
-            num_caught_anoms = len(outliers.loc[outliers.truth_bit == 1])
-            tpr = num_caught_anoms / positives
-            fpr = (len(outliers) - num_caught_anoms) / negatives
-            cut_props['cut'].append(cut)
-            cut_props['tpr'].append(tpr)
-            cut_props['fpr'].append(fpr)
 
         x_lab = '$m_{jj}$ [GeV]'
         bins = np.linspace(1000, 6000, 51)
-        make_bump_graph(all_dijet_mass, outlier_dijet_mass, x_lab, dijet_graph_name, bins, output_dir)
+        make_bump_graph(nonoutlier_dijet_mass, outlier_dijet_mass, x_lab, dijet_graph_name, bins, output_dir)
 
         # make graph for mj1
         all_m1_mass = df['mass1']
+        nonoutlier_m1_mass = nonoutliers['mass1']
         outlier_m1_mass = outliers['mass1']
         x_lab = '$m_{j1}$ [GeV]'
         bins = np.linspace(0, 1800, 51)
-        make_bump_graph(all_m1_mass, outlier_m1_mass, x_lab, mj1_graph_name, bins, output_dir)
+        make_bump_graph(nonoutlier_m1_mass, outlier_m1_mass, x_lab, mj1_graph_name, bins, output_dir)
 
         # make graph for mj2
         all_m2_mass = df['mass2']
+        nonoutlier_m2_mass = nonoutliers['mass2']
         outlier_m2_mass = outliers['mass2']
         x_lab = '$m_{j2}$ [GeV]'
         bins = np.linspace(0, 1800, 51)
-        make_bump_graph(all_m2_mass, outlier_m2_mass, x_lab, mj2_graph_name, bins, output_dir)
+        make_bump_graph(nonoutlier_m2_mass, outlier_m2_mass, x_lab, mj2_graph_name, bins, output_dir)
 
     if bb == 'rnd':  # plot roc for rnd set
-        tpr = cut_props['tpr']
-        fpr = cut_props['fpr']
-        auc = metrics.auc(tpr, fpr)
+        df['loss_sum'] = df['loss1']+df['loss2']
+        df['loss_min'] = np.minimum(df['loss1'],df['loss2'])
+        df['loss_max'] = np.maximum(df['loss1'],df['loss2'])
+
         plt.figure(figsize=(6,4.4))
-        lw = 2
-        plt.plot(fpr, tpr, color='darkorange',
-                     lw=lw, label='ROC curve (area = %0.2f)' % auc)
+        lw = 2            
         plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.legend(loc="lower right")
+
+        for var in ['sum', 'min', 'max']:
+            fpr, tpr, thresholds = metrics.roc_curve(df['truth_bit'],df['loss_'+var])
+            auc = metrics.auc(fpr, tpr)
+            plt.plot(fpr, tpr, 
+                     lw=lw, label='ROC curve loss %s (area = %0.2f)' % (var,auc))
+            plt.xlim([0.0, 1.0])
+            plt.ylim([0.0, 1.0])
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.legend(loc="lower right")
         plt.savefig(osp.join(output_dir,savedir+'_roc.pdf'))
         plt.close()
 
@@ -324,6 +320,7 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", type=str, help="Output directory for files.", required=False, default='/anomalyvol/figures/')
     parser.add_argument("--model_num", type=int, help="0 = EdgeConv, 1 = MetaLayer", required=True)
     parser.add_argument("--use_sparseloss", action='store_true', help="Toggle use of sparseloss. Default False.", default=False, required=False)
+    parser.add_argument("--overwrite", action='store_true', help="Toggle overwrite of pkl. Default False.", default=False, required=False)
     parser.add_argument("--num_events", type=int, help="How many events to process (multiple of 100). Default 1mil", default=1000000, required=False)
     parser.add_argument("--latent_dim", type=int, help="How many units for the latent space (def=2)", default=2, required=False)
     args = parser.parse_args()
@@ -341,35 +338,55 @@ if __name__ == "__main__":
     num_events = args.num_events
     latent_dim = args.latent_dim
     output_dir = args.output_dir
+    overwrite = args.overwrite
 
     Path(output_dir).mkdir(exist_ok=True) # make a folder for the graphs of this model   
     Path(osp.join(output_dir,model_fname)).mkdir(exist_ok=True) # make a folder for the graphs of this model
+
+    cuts = np.arange(0.2, 1.0, 0.1)
 
     num_files = int(10000 - (10000 * (1100000 - num_events) / 1100000)) # how many files to read
     if num_events == 1000000:    # account for fact rnd set has 100k more events then other boxes
         num_files = int(10000 - (10000 * (1100000 - (num_events + 100000)) / 1100000)) # how many files to read
     ignore_files = 10000 - num_files
 
+
+    def get_df(proc_jets):
+        d = {'loss1': proc_jets[:,0],
+             'loss2': proc_jets[:,1],
+             'dijet_mass': proc_jets[:,2],
+             'mass1': proc_jets[:,3],
+             'mass2': proc_jets[:,4],
+             'truth_bit': proc_jets[:,5]}
+        df = pd.DataFrame(d)
+        return df
+    
     print("Plotting RnD set")
-    cuts = np.arange(0.90, 1.0, 0.01)
     bb4 = GraphDataset('/anomalyvol/data/lead_2/rnd/', bb=4)
     torch.manual_seed(0) # consistency for random_split
     bb4, ignore, ignore2 = random_split(bb4, [num_files, ignore_files, 0])
     bb4_loader = DataListLoader(bb4)
-    proc_jets, input_fts, reco_fts = process(bb4_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
+    if not osp.isfile(osp.join(output_dir,model_fname,'rnd','df.pkl')) or overwrite:
+        proc_jets, input_fts, reco_fts = process(bb4_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
+        df = get_df(proc_jets)
+        df.to_pickle(osp.join(output_dir,model_fname,'rnd','df.pkl'))
+    else:
+        df = pd.read_pickle(osp.join(output_dir,model_fname,'rnd','df.pkl'))
     #plot_reco_difference(input_fts, reco_fts, model_fname, 'bb4', output_dir)  # plot reconstruction difference
-    bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, 'rnd', output_dir)  # plot bump hunts
-
-    sys.exit()
+    bump_hunt(df, cuts, model_fname, model_num, use_sparseloss, 'rnd', output_dir)  # plot bump hunts
 
     num_files = int(10000 - (10000 * (1000000 - num_events) / 1000000)) # how many files to read
     ignore_files = 10000 - num_files
     for i, bb_name in enumerate(["bb0", "bb1", "bb2"]):
         print("Plotting %s"%bb)
-        cuts = np.arange(0.99, 0.999, 0.001)
         bb = GraphDataset('/anomalyvol/data/lead_2/%s/'%bb_name, bb=i)
         bb, ignore, ignore2 = random_split(bb0, [num_files, ignore_files, 0])
         bb_loader = DataListLoader(bb)
-        proc_jets, input_fts, reco_fts = process(bb_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
+        if not osp.isfile(osp.join(output_dir,model_fname,bb,'df.pkl')) or overwrite:
+            proc_jets, input_fts, reco_fts = process(bb_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m                                                                                 
+            df = get_df(proc_jets)
+            df.to_pickle(osp.join(output_dir,model_fname,bb,'df.pkl'))
+        else:
+            df = pd.read_pickle(osp.join(output_dir,model_fname,bb,'df.pkl'))
         #plot_reco_difference(input_fts, reco_fts, model_fname, bb_name, output_dir)
-        bump_hunt(proc_jets, cuts, model_fname, model_num, use_sparseloss, bb_name, output_dir)
+        bump_hunt(df, cuts, model_fname, model_num, use_sparseloss, bb_name, output_dir)
