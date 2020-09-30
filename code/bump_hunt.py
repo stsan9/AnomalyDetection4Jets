24d4f5838507e24cc8c8a52c8a03345337ccbaf0
stsan9 <stsan@ucsd.edu>
2020-09-30 18:12:07 +0000

    black box selectable

--- code/bump_hunt.py
+++ code/bump_hunt.py
@@ -17,6 +17,7 @@ import awkward
 from pathlib import Path
 from sklearn import metrics
 import sys
+import math
 
 def invariant_mass(jet1_e, jet1_px, jet1_py, jet1_pz, jet2_e, jet2_px, jet2_py, jet2_pz):
     """
@@ -303,6 +304,7 @@ if __name__ == "__main__":
     parser.add_argument("--overwrite", action='store_true', help="Toggle overwrite of pkl. Default False.", default=False, required=False)
     parser.add_argument("--num_events", type=int, help="How many events to process (multiple of 100). Default 1mil", default=1000000, required=False)
     parser.add_argument("--latent_dim", type=int, help="How many units for the latent space (def=2)", default=2, required=False)
+    parser.add_argument("--box_num", type=int, help="0=QCD-background; 1=bb1; 2=bb2; 4=rnd", required=True)
     args = parser.parse_args()
 
     # validate arguments
@@ -312,6 +314,8 @@ if __name__ == "__main__":
         exit("--model_num can only be 0 (EdgeNet) or 1 (MetaLayer)")
     if args.latent_dim <= 0:
         exit("--latent_dim must be greater than 0")
+    if args.box_num not in [0, 1, 2, 4]:
+        exit("--box_num invalid; must be 0, 1, 2, or 4")
     model_fname = args.model_name
     model_num = args.model_num
     use_sparseloss = args.use_sparseloss
@@ -319,17 +323,11 @@ if __name__ == "__main__":
     latent_dim = args.latent_dim
     output_dir = args.output_dir
     overwrite = args.overwrite
+    box_num = args.box_num
+    cuts = np.arange(0.2, 1.0, 0.1)
 
     Path(output_dir).mkdir(exist_ok=True) # make a folder for the graphs of this model   
     Path(osp.join(output_dir,model_fname)).mkdir(exist_ok=True) # make a folder for the graphs of this model
-
-    cuts = np.arange(0.2, 1.0, 0.1)
-
-    num_files = int(10000 - (10000 * (1100000 - num_events) / 1100000)) # how many files to read
-    if num_events == 1000000:    # account for fact rnd set has 100k more events then other boxes
-        num_files = int(10000 - (10000 * (1100000 - (num_events + 100000)) / 1100000)) # how many files to read
-    ignore_files = 10000 - num_files
-
 
     def get_df(proc_jets):
         d = {'loss1': proc_jets[:,0],
@@ -341,36 +339,33 @@ if __name__ == "__main__":
         df = pd.DataFrame(d)
         return df
     
-    print("Plotting RnD set")
-    savedir = osp.join(model_fname, 'rnd')
-    Path(osp.join(output_dir,savedir)).mkdir(exist_ok=True) # make a subfolder
-    bb4 = GraphDataset('/anomalyvol/data/lead_2/rnd/', bb=4)
-    torch.manual_seed(0) # consistency for random_split
-    bb4, ignore, ignore2 = random_split(bb4, [num_files, ignore_files, 0])
-    bb4_loader = DataListLoader(bb4)
-    if not osp.isfile(osp.join(output_dir,model_fname,'rnd','df.pkl')) or overwrite:
-        proc_jets, input_fts, reco_fts = process(bb4_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
-        df = get_df(proc_jets)
-        df.to_pickle(osp.join(output_dir,model_fname,'rnd','df.pkl'))
+    # how many files to read
+    max_files = 10000   # each file has 100 events (~240 jets)
+    if box_num == 4:
+        # account for fact rnd set has 100k more events then other boxes
+        num_files = int(max_files - math.floor(max_files * (1100000 - num_events) / 1100000))
+        print(num_files)
+        if num_events == 1000000:    
+            num_files = int(max_files - (max_files * (1100000 - (num_events + 100000)) / 1100000)) # how many files to read
+        ignore_files = max_files - num_files
     else:
-        df = pd.read_pickle(osp.join(output_dir,model_fname,'rnd','df.pkl'))
-    #plot_reco_difference(input_fts, reco_fts, model_fname, 'bb4', output_dir)  # plot reconstruction difference
-    bump_hunt(df, cuts, model_fname, model_num, use_sparseloss, 'rnd', output_dir)  # plot bump hunts
+        num_files = int(max_files - (max_files * (1000000 - num_events) / 1000000))
+        print(num_files)
+        ignore_files = max_files - num_files
 
-    num_files = int(10000 - (10000 * (1000000 - num_events) / 1000000)) # how many files to read
-    ignore_files = 10000 - num_files
-    for i, bb_name in enumerate(["bb0", "bb1", "bb2"]):
-        print("Plotting %s"%bb_name)
-        savedir = osp.join(model_fname, bb_name)
-        Path(osp.join(output_dir,savedir)).mkdir(exist_ok=True) # make a subfolder
-        bb = GraphDataset('/anomalyvol/data/lead_2/%s/'%bb_name, bb=i)
-        bb, ignore, ignore2 = random_split(bb, [num_files, ignore_files, 0])
-        bb_loader = DataListLoader(bb)
-        if not osp.isfile(osp.join(output_dir,model_fname,bb_name,'df.pkl')) or overwrite:
-            proc_jets, input_fts, reco_fts = process(bb_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
-            df = get_df(proc_jets)
-            df.to_pickle(osp.join(output_dir,model_fname,bb_name,'df.pkl'))
-        else:
-            df = pd.read_pickle(osp.join(output_dir,model_fname,bb_name,'df.pkl'))
-        #plot_reco_difference(input_fts, reco_fts, model_fname, bb_name, output_dir)
-        bump_hunt(df, cuts, model_fname, model_num, use_sparseloss, bb_name, output_dir)
+    bb_name = ["bb0", "bb1", "bb2", "bb3", "rnd"][box_num]
+    print("Plotting %s"%bb_name)
+    savedir = osp.join(model_fname, bb_name)
+    Path(osp.join(output_dir,savedir)).mkdir(exist_ok=True) # make a subfolder
+    bb = GraphDataset('/anomalyvol/data/lead_2/%s/'%bb_name, bb=box_num)
+    torch.manual_seed(0) # consistency for random_split
+    bb, ignore, ignore2 = random_split(bb, [num_files, ignore_files, 0])
+    bb_loader = DataListLoader(bb)
+    if not osp.isfile(osp.join(output_dir,model_fname,bb_name,'df.pkl')) or overwrite:
+        proc_jets, input_fts, reco_fts = process(bb_loader, num_events, model_fname, model_num, use_sparseloss, latent_dim) # colms: loss1, loss2, dijet_m, jet1_m, jet2_m
+        df = get_df(proc_jets)
+        df.to_pickle(osp.join(output_dir,model_fname,bb_name,'df.pkl'))
+    else:
+        df = pd.read_pickle(osp.join(output_dir,model_fname,bb_name,'df.pkl'))
+    #plot_reco_difference(input_fts, reco_fts, model_fname, bb_name, output_dir)
+    bump_hunt(df, cuts, model_fname, model_num, use_sparseloss, bb_name, output_dir)
