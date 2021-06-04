28df71d1795b450cea423045aca14b57719aea66
stsan9 <stsan@ucsd.edu>
2021-06-03 23:38:34 -0700

    batch size flag

--- code/test_deepemd.py
+++ code/test_deepemd.py
@@ -10,41 +10,47 @@ from loss_util import LossFunction
 from graph_data import GraphDataset
 from torch_geometric.data import DataLoader
 
-# load data
-gdata = GraphDataset(root='/anomalyvol/data/tiny2',bb=0)
-data = []
-for d in gdata: # break down files
-    data += d
-device = 'cuda:0'
-batch_size = 16
-loader = DataLoader(data, batch_size=batch_size, pin_memory=True, shuffle=False)
-deepemd = LossFunction('deep_emd_loss', device=device)
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch-size", type=int, help="batch size", default=8, required=False)
+    args = parser.parse_args()
+    batch_size = args.batch_size
 
-# calculate emds
-losses = []
-t = tqdm.tqdm(loader,total=len(data)/batch_size)
-for b in t:
-    b.to(device)
-    loss = deepemd.loss_ftn(b.x, b.x, b.batch) # reformats data before feeding into emd_loss
-    losses += loss.tolist()
+    # load data
+    gdata = GraphDataset(root='/anomalyvol/data/tiny2',bb=0)
+    data = []
+    for d in gdata: # break down files
+        data += d
+    device = 'cuda:0'
+    loader = DataLoader(data, batch_size=batch_size, pin_memory=True, shuffle=False)
+    deepemd = LossFunction('deep_emd_loss', device=device)
 
-losses = np.array(losses)
-np.save('/anomalyvol/info/deepemdlosses', losses)
+    # calculate emds
+    losses = []
+    t = tqdm.tqdm(loader,total=len(data)/batch_size)
+    for b in t:
+        b.to(device)
+        loss = deepemd.loss_ftn(b.x, b.x, b.batch) # reformats data before feeding into emd_loss
+        losses += loss.tolist()
 
-# analysis
-max_emd = np.around(max(losses), decimals=3)
-mu = np.format_float_scientific(np.mean(losses), precision=3)
-sigma = np.format_float_scientific(np.std(losses), precision=3)
+    losses = np.array(losses)
+    np.save('/anomalyvol/info/deepemdlosses', losses)
 
-# plot
-plt.figure(figsize=(7,5.8))
-hts,bins,_=plt.hist(losses,bins=100)
-plt.xlabel("EMD", fontsize=16)
-x = min(bins)
-y = max(hts) * 0.8
-plt.text(x, y, f'$\mu={mu}$'
-               '\n'
-               f'$\sigma={sigma}$'
-               '\n'
-               f'$max={max_emd}$')
-plt.savefig('/anomalyvol/info/emd_losses.png')
+    # analysis
+    max_emd = np.around(max(losses), decimals=3)
+    mu = np.format_float_scientific(np.mean(losses), precision=3)
+    sigma = np.format_float_scientific(np.std(losses), precision=3)
+
+    # plot
+    plt.figure(figsize=(7,5.8))
+    hts,bins,_=plt.hist(losses,bins=100)
+    plt.xlabel("EMD", fontsize=16)
+    x = min(bins)
+    y = max(hts) * 0.8
+    plt.text(x, y, f'$\mu={mu}$'
+                '\n'
+                f'$\sigma={sigma}$'
+                '\n'
+                f'$max={max_emd}$')
+    plt.savefig('/anomalyvol/info/emd_losses.png')
