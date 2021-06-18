590cb27d0f8c0e0a3c7fa8d2c04251e8447567f5
stsan9 <stsan@ucsd.edu>
2021-06-18 20:25:22 +0000

    simpler names

--- code/emd_models.py
+++ code/emd_models.py
@@ -61,9 +61,9 @@ class DeeperDynamicEdgeNet(nn.Module):
         data.u = torch.cat([u1, u2],dim=-1)       
         return self.outnn(data.u)
 
-class SymmetricDDEdgeNet(nn.Module):
+class EmdNN(nn.Module):
     def __init__(self, input_dim=4, big_dim=32, bigger_dim=128, global_dim=2, output_dim=1, k=16, aggr='mean', device='cuda:0'):
-        super(SymmetricDDEdgeNet, self).__init__()
+        super(EmdNN, self).__init__()
         self.EdgeNet = DeeperDynamicEdgeNet(input_dim, big_dim, bigger_dim, global_dim, output_dim, k, aggr).to(device)
 
     def forward(self, data):
@@ -77,9 +77,9 @@ class SymmetricDDEdgeNet(nn.Module):
         loss = (emd_1 + emd_2) / 2
         return loss, emd_1, emd_2
 
-class SymmetricDDEdgeNetSpl(nn.Module):
+class EmdNNSpl(nn.Module):
     def __init__(self, input_dim=4, big_dim=32, bigger_dim=128, global_dim=2, output_dim=1, k=16, aggr='mean', device='cuda:0'):
-        super(SymmetricDDEdgeNetSpl, self).__init__()
+        super(EmdNNSpl, self).__init__()
         self.EdgeNet = DeeperDynamicEdgeNet(input_dim, big_dim, bigger_dim, global_dim, output_dim, k, aggr).to(device)
 
     def forward(self, data):
@@ -94,9 +94,9 @@ class SymmetricDDEdgeNetSpl(nn.Module):
         loss = (emd_1 + emd_2) / 2
         return loss, emd_1, emd_2
 
-class SymmetricDDEdgeNetRel(nn.Module):
+class EmdNNRel(nn.Module):
     def __init__(self, input_dim=4, big_dim=32, bigger_dim=128, global_dim=2, output_dim=1, k=16, aggr='mean', device='cuda:0'):
-        super(SymmetricDDEdgeNetRel, self).__init__()
+        super(EmdNNRel, self).__init__()
         self.EdgeNet = DeeperDynamicEdgeNet(input_dim, big_dim, bigger_dim, global_dim, output_dim, k, aggr).to(device)
 
     def forward(self, data):
