d07087d06db48edb48589b8ea9573bd405cdee5e
stsan9 <stsan@ucsd.edu>
2021-07-23 20:54:36 +0000

    torch.no_grad() for inference

--- code/plot_util.py
+++ code/plot_util.py
@@ -48,6 +48,7 @@ def plot_reco_difference(input_fts, reco_fts, save_path=None):
             plt.savefig(osp.join(save_path, feat[i] + '.pdf'))
             plt.close()
 
+@torch.no_grad()
 def gen_in_out(model, loader, device):
     input_fts = []
     reco_fts = []
