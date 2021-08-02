ac2570732681c80e1552c8e16104c511b30b7f7e
stsan9 <stsan@ucsd.edu>
2021-08-02 00:32:54 -0700

    use same processing code for emd-training and gae; need to test

---
+++ code/datagen/gdata_emd.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-dir", type=str, help="Directory to process data", required=False,
+                        default='/energyflowvol/datasets/')
+    parser.add_argument("--lhco-back", action="store_true", help="Start from tail end of lhco data to get unused dataset", required=False)
+    parser.add_argument("--n-jets", type=int, help="number of jets", required=False, default=100)
+    parser.add_argument("--n-events-merge", type=int, help="number of events to merge", required=False, default=1)
+    args = parser.parse_args()
+
+    os.makedirs(args.input_dir,exist_ok=True)
+
+    # log arguments
+    import logging
+    logging.basicConfig(filename=osp.join(args.input_dir, "logs.log"), filemode='w', level=logging.DEBUG, format='%(asctime)s | %(levelname)s: %(message)s')
+    for arg, value in sorted(vars(args).items()):
+            logging.info("Argument %s: %r", arg, value)
+
+    gdata = GraphDataset(root=args.input_dir, n_jets=args.n_jets, n_events_merge=args.n_events_merge, lhco_back=args.lhco_back)
---
+++ code/datagen/gdata_gae.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, help="dataset path", required=True)
+    parser.add_argument("--n-proc", type=int, default=1, help="number of concurrent processes")
+    parser.add_argument("--n-events", type=int, default=-1, help="number of events (-1 means all)")
+    parser.add_argument("--n-particles", type=int, default=-1, help="max number of particles per jet with zero-padding (-1 means all)")
+    parser.add_argument("--bb", type=int, default=0, help="black box number (0 is background, -1 is the mixed rnd set)")
+    parser.add_argument("--n-events-merge", type=int, default=100, help="number of events to merge")
+    args = parser.parse_args()
+
+    gdata = GraphDataset(root=args.dataset, bb=args.bb, n_proc=args.n_proc,
+                         n_events=args.n_events, n_particles=args.n_particles,
+                         n_events_merge=args.n_events_merge)
